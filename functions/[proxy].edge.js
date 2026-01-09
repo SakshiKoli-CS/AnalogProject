dc20e638140f6ac103afee0f13084ef870ec9187
SakshiKoli-CS <sakshi.koli@contentstack.com>
2026-01-09 16:57:04 +0530

    return 500 on every question

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,19 +1,3 @@
 export default async function handler(request, context) {
-    const url = new URL(request.url);
-  
-    console.log("Request received at:", url.pathname);
-  
-    // Redirect /csr to /about-us
-    if (url.pathname === "/csr") {
-      return Response.redirect(new URL("/about-us", url.origin), 301);
-    }
-  
-    if (url.pathname === "/legacy") {
-      return new Response(
-        JSON.stringify({ message: "Hello from Edge Function!", time: new Date() }),
-        { status: 200, headers: { "Content-Type": "application/json" } }
-      );
-    }
-  
-    return fetch(request);
+    return new Response("Internal Server Error", { status: 500 });
   }
